27cdd1d5668d663c8b659cb4166d0b5272a6e7f4
Yiqi Yang (杨弋奇) <yiqi.notfound@gmail.com>
2026-05-12 13:53:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,196 @@
+[![Version](https://vsmarketplacebadges.dev/version-short/orangex4.hsnips.png)](https://marketplace.visualstudio.com/items?itemName=orangex4.hsnips)
+[![Rating](https://vsmarketplacebadges.dev/rating-short/orangex4.hsnips.png)](https://marketplace.visualstudio.com/items?itemName=orangex4.hsnips)
+[![Installs](https://vsmarketplacebadges.dev/installs/orangex4.hsnips.png)](https://marketplace.visualstudio.com/items?itemName=orangex4.hsnips)
+
+# HyperSnips for Math
+
+这是一个由 [OrangeX4]() 魔改过的 HyperSnips, 增加了**对 Markdown 和 Latex 中数学环境匹配** 的功能. 并且加入了 `${VISUAL}` 语法的支持.
+
+GitHub 地址: https://github.com/OrangeX4/hsnips
+
+**使用这个插件前, 请把原来的 HyperSnips 插件删除!**
+**使用这个插件前, 请把原来的 HyperSnips 插件删除!**
+**使用这个插件前, 请把原来的 HyperSnips 插件删除!**
+
+并且 **开启** 在 markdown 下的 **自动补全提示**, 请使用 `Shift + Ctrl + P` 然后输入 `open settings json` 打开配置文件, 然后加入以下部分:
+
+```json
+"[markdown]": {
+    "editor.quickSuggestions": true
+},
+```
+
+**安装完成后**, 按下快捷键 `Ctrl + Shift + P`, 输入命令 `Open Snippets Directory`, 就可以打开一个文件夹. 在 **该文件夹** 新建一个文件 `markdown.hsnips`, 并将 [OrangeX4's hsnips](https://github.com/OrangeX4/OrangeX4-HyperSnips/blob/main/markdown.hsnips) 里面的内容输入进去, 保存, 就可以使用了.
+
+先看个 **普通例子**:
+
+```hsnips
+snippet RR "R" iAm
+\mathbb{R}
+endsnippet
+```
+
+这是一个在数学环境中自动展开的 Snippet, 它有三个标示符 `iAm`, 分别代表 "在词语内部也会触发", "自动展开" 和 "数学环境".
+
+这个例子会在数学环境内, 自动将 `RR` 展开成为 `\mathbb{R}`, 代表 "实数".
+
+再看个 **正则表达式** 的例子:
+
+``` hsnips
+snippet `((\d+)|(\d*)(\\)?([A-Za-z]+)((\^|_)(\{\d+\}|\d))*)/` "Fraction no ()" Am
+\frac{``rv = m[1]``}{$1}$0
+endsnippet
+```
+
+其中 `rv = m[1]` 是 JavaScript 代码, 表示将正则表达式的第一个组 `m[1]` 输出给 "返回值" `rv`, 然后输出出去.
+
+这是一个在数学环境中自动展开的 Snippet, 它有两个标示符 'Am', 分别代表 '自动展开' 和 '数学环境'. 用处是:
+
+```
+1/    --->    \frac{1}{}
+```
+
+相比于原来的 HyperSnips, 最大特点是, 它只会在数学环境 `$...$`, `$$...$$`, `\(...\)` 和 `\[...\]` 中自动展开!
+
+**还有 `${VISUAL}` 语法:**
+
+```
+snippet fr "frac" iAm
+\\frac{${1:${VISUAL}}}{$2}
+endsnippet
+```
+
+这个语法会保存最近选中的内容 (5 秒内), 然后替换掉 `${VISUAL}` 部分.
+
+以下是原来的 `README.md`:
+
+---
+
+![](https://github.com/OrangeX4/hsnips/raw/HEAD/images/welcome.gif)
+
+HyperSnips is a snippet engine for vscode heavily inspired by vim's
+[UltiSnips](https://github.com/SirVer/ultisnips).
+
+## Usage
+
+To use HyperSnips you create `.hsnips` files on a directory which depends on your platform:
+
+- Windows: `%APPDATA%\Code\User\hsnips\(language).hsnips`
+- Mac: `$HOME/Library/Application Support/Code/User/hsnips/(language).hsnips`
+- Linux: `$HOME/.config/Code/User/hsnips/(language).hsnips`
+
+Or alternatively, you can open this directory by running the command `HyperSnips: Open snippets directory`.
+
+Additionally, you can create an `all.hsnips` file for snippets that should be available on all languages.
+
+### Snippets file
+
+A snippets file is a file with the `.hsnips` extension, the file is composed of two types of blocks:
+global blocks and snippet blocks.
+
+Global blocks are JavaScript code blocks with code that is shared between all the snippets defined
+in the current file. They are defined with the `global` keyword, as follows:
+
+```hsnips
+global
+// JavaScript code
+endglobal
+```
+
+Snippet blocks are snippet definitions. They are defined with the `snippet` keyword, as follows:
+
+```hsnips
+snippet trigger "description" flags
+body
+endsnippet
+```
+
+where the `trigger` field is required and the fields `description` and `flags` are optional.
+
+### Trigger
+
+A trigger can be any sequence of characters which does not contain a space, or a regular expression
+surrounded by backticks (`` ` ``).
+
+### Flags
+
+The flags field is a sequence of characters which modify the behavior of the snippet, the available
+flags are the following:
+
+- `A`: Automatic snippet expansion - Usually snippets are activated when the `tab` key is pressed,
+  with the `A` flag snippets will activate as soon as their trigger matches, it is specially useful
+  for regex snippets.
+
+- `i`: In-word expansion\* - By default, a snippet trigger will only match when the trigger is
+  preceded by whitespace characters. A snippet with this option is triggered regardless of the
+  preceding character, for example, a snippet can be triggered in the middle of a word.
+
+- `w`: Word boundary\* - With this option the snippet trigger will match when the trigger is a word
+  boundary character. Use this option, for example, to permit expansion where the trigger follows
+  punctuation without expanding suffixes of larger words.
+
+- `b`: Beginning of line expansion\* - A snippet with this option is expanded only if the
+  tab trigger is the first word on the line. In other words, if only whitespace precedes the tab
+  trigger, expand.
+
+- `M`: Multi-line mode - By default, regex matches will only match content on the current line, when
+  this option is enabled the last `hsnips.multiLineContext` lines will be available for matching.
+
+- `m`: Math mode
+
+\*: This flag will only affect snippets which have non-regex triggers.
+
+### Snippet body
+
+The body is the text that will replace the trigger when the snippet is expanded, as in usual
+snippets, the tab stops `$1`, `$2`, etc. are available.
+
+The full power of HyperSnips comes when using JavaScript interpolation: you can have code blocks
+inside your snippet delimited by two backticks (` `` `) that will run when the snippet is expanded,
+and every time the text in one of the tab stops is changed.
+
+### Code interpolation
+
+Inside the code interpolation, you have access to a few special variables:
+
+- `rv`: The return value of your code block, the value of this variable will replace the code block
+  when the snippet is expanded.
+- `t`: An array containing the text within the tab stops, in the same order as the tab stops are
+  defined in the snippet block. You can use it to dynamically change the snippet content.
+- `m`: An array containing the match groups of your regular expression trigger, or an empty array if
+  the trigger is not a regular expression.
+- `w`: A URI string of the currently opened workspace, or an empty string if no workspace is open.
+- `path`: A URI string of the current document. (untitled documents have the scheme `untitled`)
+
+Additionally, every variable defined in one code block will be available in all the subsequent code
+blocks in the snippet.
+
+The `require` function can also be used to import NodeJS modules.
+
+## Examples
+
+- Simple snippet which greets you with the current date and time
+
+```hsnips
+snippet dategreeting "Gives you the current date!"
+Hello from your hsnip at ``rv = new Date().toDateString()``!
+endsnippet
+```
+
+- Box snippet as shown in the gif above
+
+```hsnips
+snippet box "Box" A
+``rv = '┌' + '─'.repeat(t[0].length + 2) + '┐'``
+│ $1 │
+``rv = '└' + '─'.repeat(t[0].length + 2) + '┘'``
+endsnippet
+```
+
+- Snippet to insert the current filename
+
+```hsnips
+snippet filename "Current Filename"
+``rv = require('path').basename(path)``
+endsnippet
+```
---
+++ src/extension.ts
@@ -0,0 +1,299 @@
+import * as vscode from 'vscode';
+import { existsSync, mkdirSync, readdirSync, readFileSync } from 'fs';
+import * as path from 'path';
+import { openExplorer } from './openFileExplorer';
+import { HSnippet } from './hsnippet';
+import { HSnippetInstance } from './hsnippetInstance';
+import { parse } from './parser';
+import { getSnippetDir } from './utils';
+import { getCompletions, CompletionInfo } from './completion';
+
+const SNIPPETS_BY_LANGUAGE: Map<string, HSnippet[]> = new Map();
+const SNIPPET_STACK: HSnippetInstance[] = [];
+
+let insertingSnippet = false;
+
+async function loadSnippets() {
+    SNIPPETS_BY_LANGUAGE.clear();
+
+    let snippetDir = getSnippetDir();
+    if (!existsSync(snippetDir)) {
+        mkdirSync(snippetDir);
+    }
+
+    for (let file of readdirSync(snippetDir)) {
+        if (path.extname(file).toLowerCase() != '.hsnips') continue;
+
+        let filePath = path.join(snippetDir, file);
+        let fileData = readFileSync(filePath, 'utf8');
+
+        let language = path.basename(file, '.hsnips').toLowerCase();
+
+        SNIPPETS_BY_LANGUAGE.set(language, parse(fileData));
+    }
+
+    let globalSnippets = SNIPPETS_BY_LANGUAGE.get('all');
+    if (globalSnippets) {
+        for (let [language, snippetList] of SNIPPETS_BY_LANGUAGE.entries()) {
+            if (language != 'all') snippetList.push(...globalSnippets);
+        }
+    }
+
+    // Sort snippets by descending priority.
+    for (let snippetList of SNIPPETS_BY_LANGUAGE.values()) {
+        snippetList.sort((a, b) => b.priority - a.priority);
+    }
+}
+
+// This function may be called after a snippet expansion, in which case the original text was
+// replaced by the snippet label, or it may be called directly, as in the case of an automatic
+// expansion. Depending on which case it is, we have to delete a different editor range before
+// triggering the real hsnip expansion.
+export async function expandSnippet(
+    completion: CompletionInfo,
+    editor: vscode.TextEditor,
+    snippetExpansion = false
+) {
+    let snippetInstance = new HSnippetInstance(
+        completion.snippet,
+        editor,
+        completion.range.start,
+        completion.groups
+    );
+
+    let insertionRange: vscode.Range | vscode.Position = completion.range.start;
+
+    // The separate deletion is a workaround for a VsCodeVim bug, where when we trigger a snippet which
+    // has a replacement range, it will go into NORMAL mode, see issues #28 and #36.
+
+    // TODO: Go back to inserting the snippet and removing in a single command once the VsCodeVim bug
+    // is fixed.
+
+    insertingSnippet = true;
+    await editor.edit(
+        (eb) => {
+            eb.delete(snippetExpansion ? completion.completionRange : completion.range);
+        },
+        { undoStopAfter: false, undoStopBefore: !snippetExpansion }
+    );
+
+    await editor.insertSnippet(snippetInstance.snippetString, insertionRange, {
+        undoStopAfter: false,
+        undoStopBefore: false,
+    });
+
+    if (snippetInstance.selectedPlaceholder != 0) SNIPPET_STACK.unshift(snippetInstance);
+    insertingSnippet = false;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    loadSnippets();
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.openSnippetsDir', () => openExplorer(getSnippetDir()))
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.openSnippetFile', async () => {
+            let snippetDir = getSnippetDir();
+            let files = readdirSync(snippetDir);
+            let selectedFile = await vscode.window.showQuickPick(files);
+
+            if (selectedFile) {
+                let document = await vscode.workspace.openTextDocument(path.join(snippetDir, selectedFile));
+                vscode.window.showTextDocument(document);
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.reloadSnippets', () => loadSnippets())
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.leaveSnippet', () => {
+            while (SNIPPET_STACK.length) SNIPPET_STACK.pop();
+            vscode.commands.executeCommand('leaveSnippet');
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.nextPlaceholder', () => {
+            if (SNIPPET_STACK[0] && !SNIPPET_STACK[0].nextPlaceholder()) {
+                SNIPPET_STACK.shift();
+            }
+            vscode.commands.executeCommand('jumpToNextSnippetPlaceholder');
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('hsnips.prevPlaceholder', () => {
+            if (SNIPPET_STACK[0] && !SNIPPET_STACK[0].prevPlaceholder()) {
+                SNIPPET_STACK.shift();
+            }
+            vscode.commands.executeCommand('jumpToPrevSnippetPlaceholder');
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.workspace.onDidSaveTextDocument((document) => {
+            if (document.languageId == 'hsnips') {
+                loadSnippets();
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand(
+            'hsnips.expand',
+            (editor, _, completion: CompletionInfo) => {
+                expandSnippet(completion, editor, true);
+            }
+        )
+    );
+
+    function isMathEnvironment(editor: vscode.TextEditor) {
+        let text = editor.document.getText(new vscode.Range(new vscode.Position(0, 0), editor.selection.start))
+        text = text.replace(/\\\$/g, ' ')
+        text = text.replace(/```[\s\S]+?```/g, '')
+        text = text.replace(/`[^`\n]+`/g, '')
+        text = text.replace(/<!--[\s\S]+?-->/g, '')
+        if (isInsideNonMathCommand(text)) {
+            return false
+        }
+        const reg = /(\\begin\{gather\*\}[^\$]*?\\end\{gather\*\})|(\\begin\{gather\}[^\$]*?\\end\{gather\})|(\\begin\{align\*\}[^\$]*?\\end\{align\*\})|(\\begin\{align\}[^\$]*?\\end\{align\})|(\\begin\{equation\*\}[^\$]*?\\end\{equation\*\})|(\\begin\{equation\}[^\$]*?\\end\{equation\})|(\\\[[^\$]*?\\\])|(\\\([^\$]*?\\\))|(\$\$[^\$]+\$\$)|(\$[^\$]+?\$)/g
+        text = text.replace(reg, '')
+        if (text.indexOf('$') == -1 && text.indexOf('\\(') == -1 && text.indexOf('\\[') == -1 && text.indexOf('\\begin{equation}') == -1 && text.indexOf('\\begin{equation*}') == -1 && text.indexOf('\\begin{align}') == -1 && text.indexOf('\\begin{align*}') == -1 && text.indexOf('\\begin{gather}') == -1 && text.indexOf('\\begin{gather*}') == -1) {
+            return false
+        } else {
+            const txt_reg = /(\\text{[^}]+})|(\\operatorname{[^}\n]+})|(\\mathrm{[^}\n]+})/g
+            text = text.replace(txt_reg, ' ')
+            if (text.indexOf('\\text{') == -1 && text.indexOf('\\operatorname{') == -1 && text.indexOf('\\mathrm{') == -1) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    function isInsideNonMathCommand(text: string) {
+        if (text.match(/\\(?:l(?:a(?:b(?:e(?:l)?)?)?)?|t(?:a(?:g)?)?)$/)) {
+            return true
+        }
+
+        const commandReg = /\\(?:label|tag)\b/g
+        let match: RegExpExecArray | null
+        while ((match = commandReg.exec(text)) !== null) {
+            let index = commandReg.lastIndex
+            while (index < text.length && /\s/.test(text[index])) {
+                index++
+            }
+            if (index >= text.length) {
+                return true
+            }
+            if (text[index] !== '{') {
+                continue
+            }
+
+            let depth = 0
+            for (; index < text.length; index++) {
+                if (text[index] === '\\') {
+                    index++
+                    continue
+                }
+                if (text[index] === '{') {
+                    depth++
+                    continue
+                }
+                if (text[index] === '}') {
+                    depth--
+                    if (depth === 0) {
+                        break
+                    }
+                }
+            }
+            if (depth > 0) {
+                return true
+            }
+        }
+        return false
+    }
+
+    // Forward all document changes so that the active snippet can update its related blocks.
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeTextDocument((e) => {
+            if (SNIPPET_STACK.length && SNIPPET_STACK[0].editor.document == e.document) {
+                SNIPPET_STACK[0].update(e.contentChanges);
+            }
+
+            if (insertingSnippet) return;
+
+            let mainChange = e.contentChanges[0];
+
+            if (!mainChange) return;
+
+            // Let's try to detect only events that come from keystrokes.
+            if (mainChange.text.length != 1) return;
+
+            let snippets = SNIPPETS_BY_LANGUAGE.get(e.document.languageId.toLowerCase());
+            if (!snippets) snippets = SNIPPETS_BY_LANGUAGE.get('all');
+            if (!snippets) return;
+
+            let mainChangePosition = mainChange.range.start.translate(0, mainChange.text.length);
+            let completions = getCompletions(e.document, mainChangePosition, snippets);
+
+            // When an automatic completion is matched it is returned as an element, we check for this by
+            // using !isArray, and then expand the snippet.
+            if (completions && !Array.isArray(completions)) {
+                let editor = vscode.window.activeTextEditor;
+                if (editor && e.document == editor.document &&
+                    (!completions.snippet.math || isMathEnvironment(editor))) {
+                    expandSnippet(completions, editor);
+                    return;
+                }
+            }
+        })
+    );
+
+    // Remove any stale snippet instances.
+    context.subscriptions.push(
+        vscode.window.onDidChangeVisibleTextEditors(() => {
+            while (SNIPPET_STACK.length) SNIPPET_STACK.pop();
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.window.onDidChangeTextEditorSelection((e) => {
+            while (SNIPPET_STACK.length) {
+                if (e.selections.some((s) => SNIPPET_STACK[0].range.contains(s))) {
+                    break;
+                }
+                SNIPPET_STACK.shift();
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider([{ scheme: 'untitled' }, { scheme: 'file' }], {
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+                let snippets = SNIPPETS_BY_LANGUAGE.get(document.languageId.toLowerCase());
+                if (!snippets) snippets = SNIPPETS_BY_LANGUAGE.get('all');
+                if (!snippets) return;
+
+                // When getCompletions returns an array it means no auto-expansion was matched for the
+                // current context, in this case show the snippet list to the user.
+                let completions = getCompletions(document, position, snippets);
+                if (completions && Array.isArray(completions)) {
+                    let editor = vscode.window.activeTextEditor;
+                    return completions.filter((c) => {
+                        if (editor && (!c.snippet.math || isMathEnvironment(editor))) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    }).map((c) => c.toCompletionItem());
+                }
+            },
+        })
+    );
+}
